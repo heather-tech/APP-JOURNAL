786a6e6a3b8ba6f1ab5e43fbeffd5be5463cd49a
heather-tech <heather.k.tech@gmail.com>
2023-07-19 08:47:31 -0400

    json file not working
    updates

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   const saveIdeas = async () => {
     try {
       const response = await fetch('./db.json', {
-        method: 'POST',
+        method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
